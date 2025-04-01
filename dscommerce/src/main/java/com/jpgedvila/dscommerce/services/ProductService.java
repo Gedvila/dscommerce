ca4467cbbc071cdbbda152860108d5f3d0a380d2
João Pedro Sani Gedvila <jpgedvila0306@gmail.com>
2025-04-01 16:58:45 -0300

    Método Post para Product, correção nos imports

--- dscommerce/src/main/java/com/jpgedvila/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/jpgedvila/dscommerce/services/ProductService.java
@@ -3,7 +3,6 @@ package com.jpgedvila.dscommerce.services;
 import com.jpgedvila.dscommerce.dto.ProductDTO;
 import com.jpgedvila.dscommerce.entities.Product;
 import com.jpgedvila.dscommerce.repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -28,5 +27,19 @@ public class ProductService {
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+       Product entity = new Product();
+
+       entity.setName(dto.getName());
+       entity.setDescription(dto.getDescription());
+       entity.setPrice(dto.getPrice());
+       entity.setImgUrl(dto.getImgUrl());
+
+       entity = repository.save(entity);
+
+       return new ProductDTO(entity);
     }
 }
